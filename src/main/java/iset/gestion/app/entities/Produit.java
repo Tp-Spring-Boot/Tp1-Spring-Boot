d3488ea71514da9e971ddd6c1d5c110beaa5426d
talel <talelmejri8@gmail.com>
2023-03-02 22:01:25 +0100

    add validate

--- src/main/java/iset/gestion/app/entities/Produit.java
+++ src/main/java/iset/gestion/app/entities/Produit.java
@@ -8,6 +8,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,9 +24,15 @@ import java.io.Serializable;
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Produit implements Serializable {
 	@Id
+	@NotBlank(message = "reference is required")
+	@Size(min = 4, max = 10)
 	private String reference;
+    @NotBlank(message = "designation is required")
+	@Size(min = 4, max = 10)
 	private String designation;
+   	@Min(1)
 	private double prix;
+	@Min(1)
 	private int quantite;
 	@ManyToOne
 	@JoinColumn(name="codeClient")
